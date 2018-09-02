2e940a20398a8cdaad2d2265ef3fb84f207181aa
LeeKyoungNam <kn.lee@quintet.co.kr>
2018-09-02 16:30:42 +0900

    ...

--- src/main/java/icignal/batch/ICignalMaketingBatchChApplication.java
+++ src/main/java/icignal/batch/ICignalMaketingBatchChApplication.java
@@ -1,12 +1,17 @@
 package icignal.batch;
 
+import java.io.PrintStream;
+
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ICignalMaketingBatchChApplication {
 
 	public static void main(String[] args) {
+			
 		SpringApplication.run(ICignalMaketingBatchChApplication.class, args);
 	}
 }
